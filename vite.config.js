31f71b94697ba4fda88b0cfa0bf6bf662009b951
HitsoKeyco <olivosergio09@gmail.com>
2024-05-30 23:00:47 -0500

    30/05/24

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/admin/',
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000 
+  }
 })
